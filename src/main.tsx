2ceff5e7b89c3799da583e9c523ce4fa891723c7
risa.muraguchi <risa.muraguchi@novel.jpn.com>
2025-10-23 20:32:38 +0900

    不要な箇所を削除

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ChakraUIProvider } from './components/ui/provider.tsx';
-import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
