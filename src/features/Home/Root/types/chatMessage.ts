6c9db2f1c541e3c6fe02012dd8317c1fb2884376
risa.muraguchi <risa.muraguchi@novel.jpn.com>
2025-10-24 13:45:08 +0900

    Prettierを適用

--- src/features/Home/Root/types/chatMessage.ts
+++ src/features/Home/Root/types/chatMessage.ts
@@ -3,4 +3,3 @@ export type ChatMessage = {
   question: string;
   answer: string;
 };
-
--- src/features/Home/Root/ui/QuestionHistory.tsx
+++ src/features/Home/Root/ui/QuestionHistory.tsx
@@ -1,22 +1,35 @@
-import { VStack, Box } from '@chakra-ui/react'
-import { type ChatMessage } from '@/features/Home/Root/types/chatMessage'
-
+import { VStack, Box } from '@chakra-ui/react';
+import { type ChatMessage } from '@/features/Home/Root/types/chatMessage';
 
 interface QuestionHistoryProps {
-  chatData: ChatMessage[] | undefined
+  chatData: ChatMessage[] | undefined;
   setSelectedChatId: (id: number | null) => void;
 }
 
-export const QuestionHistory = ({ chatData=[], setSelectedChatId }: QuestionHistoryProps) => {
+export const QuestionHistory = ({ chatData = [], setSelectedChatId }: QuestionHistoryProps) => {
   return (
     <>
       <VStack overflowY={'auto'} h={'100%'} px='16px' py='12px' align='stretch'>
         {chatData.map((chat, index) => (
-          <Box key={index} w={'100%'} p='5px 12px' borderRadius='8px' _hover={{ bg: '#c9dbef' }} cursor='pointer'>
-            <Box p={1} onClick={() => {setSelectedChatId(chat.id)}}>{chat.question}</Box>
+          <Box
+            key={index}
+            w={'100%'}
+            p='5px 12px'
+            borderRadius='8px'
+            _hover={{ bg: '#c9dbef' }}
+            cursor='pointer'
+          >
+            <Box
+              p={1}
+              onClick={() => {
+                setSelectedChatId(chat.id);
+              }}
+            >
+              {chat.question}
+            </Box>
           </Box>
         ))}
       </VStack>
     </>
-  )
-}
+  );
+};
