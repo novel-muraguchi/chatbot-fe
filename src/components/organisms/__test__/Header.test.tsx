749c254b8be121ec2bad76cd41dc5624fc413655
risa.muraguchi <risa.muraguchi@novel.jpn.com>
2025-10-28 19:29:33 +0900

    ヘッダーのテストを修正

--- src/components/organisms/__test__/Header.test.tsx
+++ src/components/organisms/__test__/Header.test.tsx
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '@/App';
 import { customRender } from '@/tests/helpers/customRender';
 import { Header } from '@/components/organisms/Header';
 
@@ -9,5 +11,17 @@ describe('Header', () => {
 
     const logoIcon = screen.getByAltText('Novel Guide');
     expect(logoIcon).toBeInTheDocument();
+  });
+
+  it('アイコンクリックでroot画面に遷移すること', async () => {
+    window.history.pushState({}, '', '/dummy');
+    const user = userEvent.setup();
+
+    customRender(<App />);
+
+    const logo = screen.getByAltText('Novel Guide');
+    await user.click(logo);
+
+    expect(await screen.findByText('Chat Page')).toBeInTheDocument();
   });
 });
