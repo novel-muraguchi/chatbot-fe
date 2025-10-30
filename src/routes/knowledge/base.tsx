9a012e849f502643253dd5a45a921fd36d7e4d5f
risa.muraguchi <risa.muraguchi@novel.jpn.com>
2025-10-30 20:19:16 +0900

    ルーティング設定を修正

--- src/routes/knowledge/base.tsx
+++ src/routes/knowledge/base.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { KnowledgeContainer } from '@/features/Knowledge/Root/KnowledgeContainer';
+import { NotFoundPresentational } from '@/features/Error/NotFound/NotFoundPresentational';
 
 export const KnowledgeBaseRouter = () => {
   return (
     <Routes>
       <Route path='/' element={<KnowledgeContainer />} />
+      <Route path='*' element={<NotFoundPresentational />} />
     </Routes>
   );
 };
